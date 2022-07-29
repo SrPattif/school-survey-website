5c9b24a47d85e7c614f251df14f7fd70e3f01150
SrPattif <omininuguxx@gmail.com>
2022-07-28 22:31:21 -0300

    Fist Commit

---
+++ model/pesquisa/responderPesquisa.php
@@ -0,0 +1,5 @@
+<div class="card mx-auto m-5" style="width: 98%;">
+    <div class="card-body">
+        body
+    </div>
+</div>
---
+++ view/pesquisa/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <!-- Meta tags Obrigatórias -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <!-- Bibliotecas Externas -->
+    <script src="https://use.fontawesome.com/8aae9daeac.js"></script>
+
+    <title>Trabalho de OT - Painel do Entrevistador</title>
+</head>
+
+<body>
+    <?php
+      $valorURL = @$_GET['p'];
+
+        if ($valorURL == 'responderPesquisa') {
+          require_once('../../model/pesquisa/responderPesquisa.php');
+
+        } else if ($valorURL == 'detalhesPesquisa') {
+          require_once('../../model/pesquisa/detalhesPesquisa.php');
+        }
+    ?>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous">
+    </script>
+</body>
+
+<footer>
+    <p class="text-center font-weight-light">Desenvolvido por Gustavo Antonio</p>
+</footer>
+
+</html>
